f8894991b8c4a513be662e3721e8ddb6bfb8ed6a
gigimic <gigimic@gmail.com>
2019-10-22 15:48:53 +0530

    web scraping

--- web_scrap.py
+++ web_scrap.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 my_url1 = 'https://elitedatascience.com/python-web-scraping-libraries'
 my_url2 = 'https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/'
 my_url3 = 'https://www.niist.res.in/english/category/contact-us'
-page = requests.get(my_url3)
+my_url4 = 'https://www.nytimes.com/books/best-sellers/hardcover-fiction/'
+page = requests.get(my_url4)
 contents = page.content
-
 
 soup = BeautifulSoup(contents, 'lxml')
 # print(soup.prettify())
@@ -28,9 +29,30 @@ print(soup.title.parent.name)
     # for title in table.find_all('li', class_='sstitle'):
     #     print(title.text)
 
-for table in soup.find_all('div', id='eventcldiv'):
-    for title in table.find_all('ul'):
-        for addres in title.find_all('li'):
-            print(addres.text)
-        print()
-        
+# for table in soup.find_all('div', id='eventcldiv'):
+#     for title in table.find_all('ul'):
+#         for addres in title.find_all('li'):
+#             print(addres.text)
+#         print()
+best_sellers = []
+for lists in soup.find_all('ol', class_='css-12yzwg4'):
+    for title in lists.find_all('li'):
+        # print(title.prettify())
+        # print(title.article.find('h3', class_='css-5pe77f'))
+        if(title.article is not None):
+            book_details = {}
+            book_name = title.article.h3.text
+            print(title.article.h3.text)
+            book_details['name'] = book_name
+            author = title.article.find('p', class_='css-1j7a9fx')
+            print(author.text)
+            book_details['author'] = author.text
+            publisher = title.article.find('p', class_='css-heg334')
+            print(publisher.text)
+            book_details['publisher'] = publisher.text
+            best_sellers.append(book_details)
+            print(title.article['itemprop'])
+            print()
+            # bookData = pd.DataFrame.from_dict(best_sellers)
+            # bookData.to_csv('list_best_sellers.csv')
+            
