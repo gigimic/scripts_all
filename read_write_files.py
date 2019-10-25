2f86822eae1380bdc8cf24a2fd9f57dc625f745c
gigimic <gigimic@gmail.com>
2019-10-25 15:52:04 +0530

    read and write files

--- read_write_files.py
+++ read_write_files.py
@@ -13,3 +13,23 @@ Loop through the file line by line:
 f = open("demofile.txt", "r")
 for x in f:
   print(x)
+
+
+Read and write csv files
+import csv
+
+file_in = 'list.csv'
+rows = []
+with open(file_in, 'r') as csv_file:
+    input_data = csv.reader(csv_file)
+    for row in input_data: 
+        rows.append(row) 
+        # print(row)
+csv_file.close()
+
+
+file_out ='list_books.csv' 
+with open(file_out, 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(rows)
+csvFile.close()
