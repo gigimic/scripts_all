78e011ba8bfb01a9b550a7a02b4b68acf02911b1
gigimic <gigimic@gmail.com>
2019-11-04 14:18:45 +0530

    updates

--- database_connect_python.py
+++ database_connect_python.py
@@ -16,18 +16,18 @@ mycursor = db.cursor()
 # mycursor.execute("CREATE TABLE student (id INT, name VARCHAR(255))")
 # mycursor.execute("CREATE TABLE employee (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), salary INT(6))")
 
-# mycursor.execute("SHOW TABLES")
-# for table in mycursor:
-#     print(table)
+mycursor.execute("SHOW TABLES")
+for table in mycursor:
+    print(table)
 
 
 # mycursor.execute("ALTER TABLE student MODIFY id INT PRIMARY KEY")
 
-student_sql_query = "INSERT INTO student(id,name) VALUES(01, 'John')"
-employee_sql_query = " INSERT INTO employee (id, name, salary) VALUES (01, 'Jane', 10000)"
+# student_sql_query = "INSERT INTO student(id,name) VALUES(01, 'John')"
+# employee_sql_query = " INSERT INTO employee (id, name, salary) VALUES (01, 'Jane', 10000)"
 #Execute cursor and pass query as well as student data
-mycursor.execute(student_sql_query)
-#Execute cursor and pass query of employee and data of employee
-mycursor.execute(employee_sql_query)
-db.commit()
+# mycursor.execute(student_sql_query)
+# #Execute cursor and pass query of employee and data of employee
+# mycursor.execute(employee_sql_query)
+# db.commit()
 print(mycursor.rowcount, "Record Inserted")
