69a0c24cddca97d275e7b37770a228c8734d51ca
gigimic <gigimic@gmail.com>
2019-11-15 11:32:28 +0530

    supervised learning

--- supervisedMl.py
+++ supervisedMl.py
@@ -53,9 +53,48 @@ print(dataset.describe())
 print(dataset.groupby('class').size())
 
 # box and whisker plots
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-pyplot.show()
+# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# pyplot.show()
 
-# histograms
-dataset.hist()
+# # histograms
+# dataset.hist()
+# pyplot.show()
+
+# scatter plot matrix
+# scatter_matrix(dataset)
+# pyplot.show()
+
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=0.20, random_state=1)
+print(len(X), len(Y), len(X_train), len(Y_train))
+
+# # Test options and evaluation metric
+# kfold = StratifiedKFold(n_splits=10, random_state=1)
+# cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
 pyplot.show()
