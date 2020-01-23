acf8daff6e7654947abe89b39aafe6859328dcef
gigimic <gigimic@gmail.com>
2020-01-23 14:32:54 +0530

    videos

--- download_video.py
+++ download_video.py
@@ -1,5 +1,13 @@
 import os
 import sys
-from pytube import YouTube
-yt = YouTube('https://www.youtube.com/watch?v=m0wIU0yvbbs&t=685s')
-yt.streams.all('/home/gigi/Downloads/')
+import pytube 
+print('imported pytube')
+print(pytube.__version__)
+yt = pytube.YouTube('https://www.youtube.com/watch?v=m0wIU0yvbbs&t=685s')
+# yt = pytube.YouTube('https://www.youtube.com/watch?v=9bZkp7q19f0')
+
+print(yt.title)
+stream = yt.streams.first()
+stream.download('/path/to/dir/')
+
+print('downloaded')
