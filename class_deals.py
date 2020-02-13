18c6e11bacb57cfeece132859ce7fd9f6c2457a5
gigimic <gigimic@gmail.com>
2020-02-13 16:06:36 +0530

    class&methods

--- class_deals.py
+++ class_deals.py
@@ -12,3 +12,26 @@ p1 = Person("John", 36)
 print(p1.name)
 print(p1.age)
 p1.myfunc()
+
+# Create a class that captures students. Each student has a first name, 
+# last name, class year, and major. Create two examples of students.
+
+class students:
+
+  def __init__(self, first, last, year, major):
+    self.first = first
+    self.last = last
+    self.year = year
+    self.major = major
+
+  def print_details(self):
+    print("student name  :", self.first, self.last)
+    print("student year  :", self.year)
+    print("student major  :", self.major)
+
+student1 = students('Monica', 'Geller', 1994, "Cooking")
+student2 = students('Rachel', 'Green', 1995, "Fashion")
+
+student1.print_details()
+student2.print_details()
+    
