b80490929eb93202a94dd9d2af67de6209602ea4
gigimic <gigimic@gmail.com>
2020-05-19 10:45:34 +0530

    webbrowser

--- web-scrape.py
+++ web-scrape.py
@@ -1,4 +1,7 @@
 # how to do web scraping
+# https://automatetheboringstuff.com/2e/chapter12/
 import webbrowser
 webbrowser.open('https://inventwithpython.com/')
-print('opened the URL')
+print('opened INVENT WITH PYTHON')
+# webbrowser.open("https://www.nytimes.com/")
+# print('opened NEW YORK TIMES')
