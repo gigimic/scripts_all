d73ad444001b624d0daa51188b39fc920f9f898a
gigimic <gigimic@gmail.com>
2020-06-02 15:25:45 +0530

    image manipulation

--- manipulate_images.py
+++ manipulate_images.py
@@ -1,6 +1,13 @@
 from PIL import ImageColor
 from PIL import Image
 
+# manipulating images in python
+# section 1: getting image details
+# section 2: cropping an image
+# section 3: create a simple image 
+# section 4: rotate and flip images
+# https://automatetheboringstuff.com/2e/chapter19/ 
+
 print(ImageColor.getcolor('red', 'RGBA'))
 print(ImageColor.getcolor('RED', 'RGBA'))
 print(ImageColor.getcolor('Black', 'RGBA'))
@@ -12,23 +19,23 @@ image_open = Image.open('peacock.jpeg')
 print(image_open.size)
 width_image, height_image = image_open.size
 
-# image details
+# section 1: image details
 print('width and height of image  ', width_image, height_image)
 print('image filename  ',image_open.filename)
 print(image_open.format)
 print(image_open.format_description)
 
-# cropping an image 
+# section 2: cropping an image 
 croppedIm = image_open.crop((70, 30, 210, 150))
 croppedIm.save('cropped.png')
 
-# creating simple images 
+# section 3: creating simple images 
 im = Image.new('RGBA', (100, 200), 'purple')
 im.save('purpleImage.png')
 im2 = Image.new('RGBA', (120, 20))
 im2.save('transparentImage.png')
 
-# rotate and flip images 
+# section 4: rotate and flip images 
 image_zebra = Image.open('zebra.jpg')
 
 image_zebra.rotate(6).save('zebra_rotated6.png')
