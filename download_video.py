ae18c603dae9b2162315d9dfcca1593a0a6186af
gigimic <gigimic@gmail.com>
2020-06-12 17:18:13 +0530

    organization of files

--- download_video.py
+++ download_video.py
@@ -3,11 +3,12 @@ import sys
 import pytube 
 print('imported pytube')
 print(pytube.__version__)
-yt = pytube.YouTube('https://www.youtube.com/watch?v=m0wIU0yvbbs&t=685s')
+# yt = pytube.YouTube('https://www.youtube.com/watch?v=m0wIU0yvbbs&t=685s')
 # yt = pytube.YouTube('https://www.youtube.com/watch?v=9bZkp7q19f0')
+yt = pytube.YouTube('https://www.youtube.com/watch?v=enYITYwvPAQ&t=3s')
 
 print(yt.title)
 stream = yt.streams.first()
-stream.download('/path/to/dir/')
-
+# stream.download('/path/to/dir/')
+stream.download('/home/gigi/Documents')
 print('downloaded')
---
+++ organize_files.py
@@ -0,0 +1,4 @@
+import shutil, os
+shutil.copy('data1.csv', 'data11.csv')
+# Here full path name can be given to copy files
+
