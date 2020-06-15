8faa0c8c68856bf93cf31e632df86f5d626eadf1
gigimic <gigimic@gmail.com>
2020-06-15 16:02:33 +0530

    organize files

--- organize_files.py
+++ organize_files.py
@@ -2,3 +2,5 @@ import shutil, os
 shutil.copy('data1.csv', 'data11.csv')
 # Here full path name can be given to copy files
 
+shutil.move('/path/from/file1', 'path/to') #moving files
+shutil.move('/path/from/file1', 'path/to/file2') # renaming files
