8f8a49cfb397c6cd3cd9a122c1735e1fb1ffa346
gigimic <gigimic@gmail.com>
2020-06-23 15:15:20 +0530

    organize files

--- download_video.py
+++ download_video.py
@@ -5,10 +5,9 @@ print('imported pytube')
 print(pytube.__version__)
 # yt = pytube.YouTube('https://www.youtube.com/watch?v=m0wIU0yvbbs&t=685s')
 # yt = pytube.YouTube('https://www.youtube.com/watch?v=9bZkp7q19f0')
-yt = pytube.YouTube('https://www.youtube.com/watch?v=enYITYwvPAQ&t=3s')
 
 print(yt.title)
 stream = yt.streams.first()
 # stream.download('/path/to/dir/')
-stream.download('/home/gigi/Documents')
+
 print('downloaded')
