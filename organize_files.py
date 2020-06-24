cbfce6e482ec964c209c24786e8288ea922d36bf
gigimic <gigimic@gmail.com>
2020-06-24 16:56:06 +0530

    organize files

--- organize_files.py
+++ organize_files.py
@@ -73,25 +73,3 @@ newZip.close()
 # 2.Call os.listdir() to find all the files in the working directory.
 # 3.Loop over each filename, using the regex to check whether it has a date.
 # 4.If it has a date, rename the file with shutil.move().
-
-import shutil, os, re
-
-   # Create a regex that matches files with the American date format.
-datePattern = re.compile(r"""^(.*?) # all text before the date
-((0|1)?\d)-                     # one or two digits for the month
-((0|1|2|3)?\d)-                 # one or two digits for the day
-((19|20)\d\d)                   # four digits for the year
-(.*?)$                          # all text after the date
-""", re.VERBOSE➌)
-
-# TODO: Loop over the files in the working directory.
-
-# TODO: Skip files without a date.
-
-# TODO: Get the different parts of the filename.
-
-# TODO: Form the European-style filename.
-
-# TODO: Get the full, absolute file paths.
-
-# TODO: Rename the files.
---
+++ rename_dates.py
@@ -0,0 +1,35 @@
+import shutil, os, re
+# Renaming Files with American-Style Dates to European-Style Dates
+# 1.Create a regex that can identify the text pattern of American-style dates.
+# 2.Call os.listdir() to find all the files in the working directory.
+# 3.Loop over each filename, using the regex to check whether it has a date.
+# 4.If it has a date, rename the file with shutil.move().
+
+# Create a regex that matches files with the American date format.
+datePattern = re.compile(r"""^(.*?) # all text before the date
+((0|1)?\d)-                     # one or two digits for the month
+((0|1|2|3)?\d)-                 # one or two digits for the day
+((19|20)\d\d)                   # four digits for the year
+(.*?)$                          # all text after the date
+""", re.VERBOSE)
+
+# print(datePattern)
+
+patt = re.compile('csv')
+for file in os.listdir('.'):
+    mo = patt.search(file)
+    if mo is not None:
+        print(file)
+
+
+# TODO: Loop over the files in the working directory.
+
+# TODO: Skip files without a date.
+
+# TODO: Get the different parts of the filename.
+
+# TODO: Form the European-style filename.
+
+# TODO: Get the full, absolute file paths.
+
+# TODO: Rename the files.
