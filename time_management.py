9ec48bf996eecc57570bd9607cd3960f6d75af45
gigimic <gigimic@gmail.com>
2020-07-06 14:33:46 +0530

    time management

--- time_management.py
+++ time_management.py
@@ -12,3 +12,23 @@ prod = calcProd()
 endTime = time.time()
 print('The result is %s digits long.' % (len(str(prod))))
 print('Took %s seconds to calculate.' % (endTime - startTime))
+
+# If you need to pause your program for a while, call 
+# the time.sleep() function and pass it the number of 
+# seconds you want your program to stay paused.
+
+for i in range(3):
+    print('Tick')
+    time.sleep(1)
+    print('Tock')
+    time.sleep(3)
+
+time.sleep(5)
+
+# Rounding Numbers
+now = time.time()
+print(now)
+print(round(now, 2))
+
+print(round(now, 4))
+print(round(now))
