af766ca20326e8e667f729207dc8a6c6951ba2c0
gigimic <gigimic@gmail.com>
2020-07-10 10:51:42 +0530

    debug

--- debug_code.py
+++ debug_code.py
@@ -29,3 +29,14 @@ try :
 except Exception as err_msg:
     print('An exception happened: ' + str(err_msg))
 
+# Python displays the traceback whenever a raised exception goes unhandled. 
+# But you can also obtain it as a string by calling traceback.format_exc(). 
+# This function is useful if you want the information from an exception’s traceback 
+# but also want an except statement to gracefully handle the exception. 
+# You will need to import Python’s traceback module before calling this function.
+
+# For example, instead of crashing your program right when an exception occurs, 
+# you can write the traceback information to a text file and keep your program running. 
+# You can look at the text file later, when you’re ready to debug your program.
+
+# logging module is more effective than simply writing this error information to text files.
