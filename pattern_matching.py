c54d41d082f263ee3e52b897cec36cc464da53f7
gigimic <gigimic@gmail.com>
2020-07-15 15:47:30 +0530

    regular expression

---
+++ pattern_matching.py
@@ -0,0 +1,9 @@
+import re
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print('Phone number found: ' +  mo.group())
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print('first part ',  mo.group(1))
