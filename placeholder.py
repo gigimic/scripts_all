78e09c23cb7ded85619beee8ff5ab120232997d6
gigimic <gigimic@gmail.com>
2020-11-20 16:16:09 +0530

    placeholder

--- placeholder.py
+++ placeholder.py
@@ -1,4 +1,5 @@
 #provides placeholders for easy initialization
+import libraries
 
 read csv file
 write csv file
@@ -9,6 +10,11 @@ write dataframe
 read from web
 
 read text file
+write as json
 
 start class
+
+define function
+open matplotlib
+open seaborn
 
