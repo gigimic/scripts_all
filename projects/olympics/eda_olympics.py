eed514143a589d9dc9d82a2c0306b5d08e4b691b
gigimic <gigimic@gmail.com>
2020-11-27 15:39:58 +0530

    eda

--- projects/olympics/eda_olympics.py
+++ projects/olympics/eda_olympics.py
@@ -4,5 +4,13 @@ print(np.__version__)
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import seaborn as sns
-print(sns.__version__)
+# import seaborn as sns
+# print(sns.__version__)
+
+data = pd.read_csv('Summer-Olympic-medals-1976-to-2008.csv', encoding='latin-1')
+# data.shape
+# rows = 15433, columns= 11
+# data.head(12)
+
+filt_athlete = data['Athlete'].str.contains('PAES', na = False)
+print(data.loc[filt_athlete, ['Athlete', 'Country', 'Medal']])
