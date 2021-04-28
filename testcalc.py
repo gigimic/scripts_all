e37f95535c349492ab34b145182f3fb00584b788
gigimic <gigimic@gmail.com>
2021-04-28 11:56:50 +0530

    update

--- testcalc.py
+++ testcalc.py
@@ -1,8 +1,13 @@
-num = [100, 100, 200, 100, 100, 200, 200]
-print(sum(num))
-price=[285.7, 215, 190.05, 166.6, 175, 136.45, 94.7]
-amt = [num[i]*price[i] for i in range(len(num))]
-print(sum(amt)) 
-sell = (400*34.45) + (600*30.1)
-loss = sum(amt) - sell
-print(loss)
+# num = [100, 100, 200, 100, 100, 200, 200]
+# print(sum(num))
+# price=[285.7, 215, 190.05, 166.6, 175, 136.45, 94.7]
+# amt = [num[i]*price[i] for i in range(len(num))]
+# print(sum(amt)) 
+# sell = (400*34.45) + (600*30.1)
+# loss = sum(amt) - sell
+# print(loss)
+
+import math
+print(math.log(1/4,2))
+a= -0.5*math.log(0.5,2)-0.5*math.log(0.5,2)
+print(a)
